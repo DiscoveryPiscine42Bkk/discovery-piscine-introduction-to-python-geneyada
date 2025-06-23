1e893d9e49c328be7450afa636ea92a34d1e38f8
geneyada <yadajensuriyarkun@gmail.com>
2025-06-23 09:07:33 +0000

    add this file

---
+++ cell04/ex03/float.py
@@ -0,0 +1,8 @@
+user_input = input("Please enter a number: ")
+try:
+    number = float(user_input)
+
+    if number % 1 !=0:
+        print("The number is a decimal.")
+except ValueError:
+    print("That's not a valid number!")
